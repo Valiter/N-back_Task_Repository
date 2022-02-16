b5e9c93d296500968894baf8dd0d599c16c199aa
antonkuzmichev <an2000n@icloud.com>
2022-02-17 02:22:16 +0300

    upd: [02:22, 17.02.22] comment: Добавил еще пару модулей — pygame_module.py и checking_inputs.py. Немного поизучал pygame и проработал общую план-схему данного модуля. План-схема не отражена нигде.
    Task's:
    - Сделать проверку стимулов, заходящих в программу.
    - Понять как вводить стимулы через pygame или же придется использовать tkinter вместе с нм и как-то совмещать... Или вообще использовать только tkinter.
    - Сделать словарь СТИМУЛ / КОМАНДА ДЛЯ ВЫВОДА в pygame_module.py.
    - Сделать список для регистрации реакций испытуемого длинной равный длинне списку стимулов.
    
     Finished Task's:
     1) Генератор ряда;
     2) анализатор сответствия интервалу;
     3) Основной файл, для вызова функций.

---
+++ pygame_module.py
@@ -0,0 +1,34 @@
+
+
+"""Тут находится отслеживание работы клавишь, а также вывод информации на экран с помозью модуля PyGame"""
+
+
+import pygame
+import sys
+
+
+def pygame_func():
+    pygame.init()
+
+    screen = pygame.display.set_mode((640, 480))
+    rect = pygame.Rect(20, 20, 50, 70)
+    color = (255, 255, 255)
+
+
+    # Обработка событий должна происходить ТОЛЬКО в цикле. И это хорошо.
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE:
+                    color = (0, 0, 0)
+
+            if event.type == pygame.KEYUP:
+                if event.key == pygame.K_SPACE:
+                    color = (255, 255, 255)
+
+        pygame.draw.rect(screen, color, rect, 0)
+        pygame.display.flip()
