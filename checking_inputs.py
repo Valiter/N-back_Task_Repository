94ece84aeafb9fa8792b66fcbe0f931956e00ed4
antonkuzmichev <an2000n@icloud.com>
2022-02-21 16:42:36 +0300

    upd: [16:42, 21.02.22] comment: Добавил изменения в checking_inputs.py и pygame_module.py.
    Task's:
    - Сделать проверку стимулов, заходящих в программу.
    - Понять как вводить стимулы через pygame или же придется использовать tkinter вместе с нм и как-то совмещать... Или вообще использовать только tkinter.
    - Сделать словарь СТИМУЛ / КОМАНДА ДЛЯ ВЫВОДА в pygame_module.py.
    - Сделать список для регистрации реакций испытуемого длинной равный длинне списку стимулов.
    
     Finished Task's:
     1) Генератор ряда;
     2) анализатор сответствия интервалу;
     3) Основной файл, для вызова функций.

--- checking_inputs.py
+++ checking_inputs.py
@@ -9,3 +9,21 @@
 
 """Название функции и объяснение её работы."""
 
+
+def checking_inputs_func(type_of_symbols_dict, count_of_using_symbols,
+                         ability_to_use_interval, length_of_fin_list, type_of_letter=2):
+    if length_of_fin_list >= ability_to_use_interval:
+        return False
+
+    elif type_of_symbols_dict > 5 or type_of_symbols_dict < 1:
+        return False
+
+    #  Проверка ниже не защищает от дурака, но хоть немного спасает от опечаток.
+    elif count_of_using_symbols > 33:
+        return False
+
+    elif type_of_letter > 2 or type_of_letter < 1:
+        return False
+
+    else:
+        return True
--- pygame_module.py
+++ pygame_module.py
@@ -1,10 +1,19 @@
 
 
-"""Тут находится отслеживание работы клавишь, а также вывод информации на экран с помозью модуля PyGame"""
+"""Тут находится отслеживание работы клавиш, а также вывод информации на экран с помощью модуля PyGame"""
 
 
 import pygame
 import sys
+
+
+'''Нижу находятся словари-расшифровщики'''
+
+figure_dict_for_n_back = {"triangle": "", "sphere": "", "square": "",
+                          "circle": "", "pyramid": "", "oval": "", "prism": ""}
+
+color_dict_for_n_back = {"white": "", "red": (255, 0, 0), "green": "", "blue": "",
+                         "yellow": "", "brown": "", "black": "", "orange": ""}
 
 
 def pygame_func():
@@ -12,9 +21,9 @@ def pygame_func():
 
     screen = pygame.display.set_mode((640, 480))
     rect = pygame.Rect(20, 20, 50, 70)
-    color = (255, 255, 255)
+    color = color_dict_for_n_back["red"]
 
-    # Обработка событий должна происходить ТОЛЬКО в цикле. И это хорошо.
+    # Обработка событий должна происходить ТОЛЬКО в цикле.
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -27,7 +36,9 @@ def pygame_func():
 
             if event.type == pygame.KEYUP:
                 if event.key == pygame.K_SPACE:
-                    color = (255, 255, 255)
+                    color = color_dict_for_n_back["red"]
 
         pygame.draw.rect(screen, color, rect, 0)
         pygame.display.flip()
+
+pygame_func()
