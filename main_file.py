a794ca75a5856450b78d32a9d2790f092ac104b3
antonkuzmichev <an2000n@icloud.com>
2022-02-21 17:55:40 +0300

    upd: [17:55, 21.02.22] comment: Подключил проверку вводимых данных. Позанимался отладкой.
    Task's:
    - Сделать проверку стимулов, заходящих в программу.
    - Понять как вводить стимулы через pygame или же придется использовать tkinter вместе с нм и как-то совмещать... Или вообще использовать только tkinter.
    - Сделать словарь СТИМУЛ / КОМАНДА ДЛЯ ВЫВОДА в pygame_module.py.
    - Сделать список для регистрации реакций испытуемого длинной равный длинне списку стимулов.
    
     Finished Task's:
     1) Генератор ряда;
     2) анализатор сответствия интервалу;
     3) Основной файл, для вызова функций.

--- main_file.py
+++ main_file.py
@@ -6,6 +6,10 @@ import generator_git
 import analyzer
 
 
+# Переменная для запуска цикла while.
+switch = True
+
+
 """Запрашиваемые данные через input()-ы выношу отдельно, чтобы было понятно, что нужно в видео части."""
 global_num_of_dict = input("Напишите число от 1-го до 5-ти: ")
 global_count_of_stimulus = int(input("Напишите количество символов используемых для создания символьного ряда: "))
@@ -17,26 +21,28 @@ interval = input("напишите длинну интервала. 1-back (11),
 
 stimulus_fin_list = 20
 
+while switch is True:
+    if checking_inputs.checking_inputs_func(int(global_num_of_dict), int(global_count_of_stimulus),
+                                            int(interval), int(stimulus_fin_list), int(type_of_letter)) is True:
 
-if checking_inputs.checking_inputs_func(int(global_num_of_dict), int(global_count_of_stimulus),
-                                        int(interval), int(stimulus_fin_list), int(type_of_letter)) is True:
+        """Вызовы функций из модуля generator_git.py"""
+        # Функция может вернуть False и тогда программа сляжет.
+        chosen_stimulus = generator_git.n_back_choosing_stimulus(global_num_of_dict,
+                                                                 global_count_of_stimulus,
+                                                                 type_of_letter)
+        end_of_thinking = generator_git.mixer_stimulus(chosen_stimulus,
+                                                       global_count_of_stimulus,
+                                                       int(stimulus_fin_list))
 
-    """Вызовы функций из модуля generator_git.py"""
-    # Функция может вернуть False и тогда программа сляжет.
-    chosen_stimulus = generator_git.n_back_choosing_stimulus(global_num_of_dict,
-                                                             global_count_of_stimulus,
-                                                             type_of_letter)
-    end_of_thinking = generator_git.mixer_stimulus(chosen_stimulus,
-                                                   global_count_of_stimulus,
-                                                   int(stimulus_fin_list))
+        """Вызовы функций из модуля analyzer.py"""
+        results_of_true_false = analyzer.working_code_reviev(end_of_thinking, interval)
 
-    """Вызовы функций из модуля analyzer.py"""
-    results_of_true_false = analyzer.working_code_reviev(end_of_thinking, interval)
+        """Вывод результатов, для понимания результатов работы программы."""
+        print(chosen_stimulus)
+        print(end_of_thinking)
+        print(results_of_true_false)
+        switch = False
 
-    """Вывод результатов, для понимания результатов работы программы."""
-    print(chosen_stimulus)
-    print(end_of_thinking)
-    print(results_of_true_false)
-
-else:
-    print("!")
+    else:
+        print("!")
+        switch = False
