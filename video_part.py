8f8cda5a42b81184fa759e1cae7b655dac6eb913
antonkuzmichev <an2000n@icloud.com>
2022-02-26 01:13:00 +0300

    upd: [01:12, 26.02.22] comment: Бинго! Хотя не могу сказать, что я рад. Получилость сделать окно, которое с ЗАПУСКА ПРОГРАММЫ!!! Начинает pygame.time.set_timer выполнять, что ведет к созданию квадрата в центре окна и смене его цвета.
    Task's:
    - Понять как вводить стимулы через pygame или же придется использовать tkinter вместе с ним и как-то совмещать... Или вообще использовать только tkinter.
    - Сделать словарь СТИМУЛ / КОМАНДА ДЛЯ ВЫВОДА в pygame_module.py.
        а) Возможно, что с фигурами стоит сделать только позиционирование? Не уверен.
        б) Настроить таймер для показа картинок.
        в) Нужно как-то сделать отображение оставшегося времени, нет?
    - Сделать список для регистрации реакций испытуемого длинной равный длинне списку стимулов.
    
     Finished Task's:
     1) Генератор ряда;
     2) анализатор сответствия интервалу;
     3) Основной файл, для вызова функций.
     4) Сделать проверку стимулов, заходящих в программу.

--- video_part.py
+++ video_part.py
@@ -10,7 +10,7 @@ def check_func():
     pygame.init()
     screen_nums = (720, 480)
 
-    color = {"green": (0, 255, 0), "blue": (0, 0, 255), "red": (255, 0, 0)}
+    color = {"green": (0, 255, 0), "blue": (0, 0, 255), "red": (255, 0, 0), "white": (255, 255, 255)}
     color_circle = {0: "red", 1: "green", 2: "blue"}
     screen = pygame.display.set_mode(screen_nums)
 
@@ -19,7 +19,6 @@ def check_func():
     start_circle = 0
     pict_changer = pygame.event.Event(pygame.USEREVENT)
 
-    pygame.draw.rect(screen, color.get('green'), (screen_nums[0] / 2 - 50, screen_nums[1] / 2 - 50, 100, 100))
     print(pict_changer)
     while True:
         for event in pygame.event.get():
@@ -28,9 +27,12 @@ def check_func():
                 pygame.quit()
                 sys.exit()
 
+            if True:
+                pygame.time.set_timer(pict_changer, 1000, False)
+
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_SPACE:
-                    pass
+                    pygame.Surface.fill(screen, color.get('white'))
 
             if event.type == pygame.USEREVENT:
                 if event == pict_changer:
@@ -44,9 +46,7 @@ def check_func():
         pygame.draw.line(screen, color.get('blue'), [screen_nums[0], 0], [0, screen_nums[1]])
         pygame.draw.line(screen, color.get('blue'), [screen_nums[0], screen_nums[1]], [0, 0])
 
-        pygame.time.set_timer(pict_changer, 3000, False)
-
-        clock.tick(30)
+        clock.tick(120)
 
         pygame.display.update()
 
